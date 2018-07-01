108f39a99595d4c4069f9d341f39f6caadb2cc8d
YotaYota_Alcedo <alcedo@outlook.jp>
2018-07-01 17:05:30 +0900

    niwakari
    鶏生成

---
+++ Assets/chikin.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class chikin
+{
+    // いじらないやつはとりあえずprivateで宣言
+    private GameObject myobj;
+
+    // 地面の枠のサイズ
+    private const int H = 50, W = 50;
+
+    // 生きてる？
+    private bool alive;
+
+    // 移動量
+    private float delta = 1;
+
+    // コンストラクタ　chikinオブジェクトを生成したときに呼び出される関数で要は初期化
+    // 引数を変えることで複数作ることも出来るよ
+    public chikin(GameObject obj)
+    {
+        myobj = obj;
+        alive = true;
+    }
+
+    // ランダムに移動させる
+    public void moveRondom()
+    {
+        Vector3 position = new Vector3(Random.Range(-delta, delta),0, Random.Range(-delta, delta));
+        myobj.transform.position += position;
+
+        // はみ出たら消す
+        if(myobj.transform.position.x > W || myobj.transform.position.z > H)
+        {
+            alive = false;
+            GameObject.Destroy(myobj);
+        }
+            
+
+    }
+
+    // alive自体をpublicにして直接参照してもいいんだけど
+    // クラスの変数はグループ開発ではprivateが無難なのでここでもこの面倒なことやってます
+    public bool isAlive()
+    {
+        return alive;
+    }
+
+    private void death()
+    {
+        GameObject.Destroy(myobj);
+    }
+}
---
+++ Assets/main.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class main : MonoBehaviour {
+
+    public GameObject chikObj;
+    private List<chikin> chikinList;
+	// Use this for initialization
+	void Start () {
+        // リストはnewしてから使う
+        chikinList = new List<chikin>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        keyInput();
+        objUpdate();
+	}
+
+    // オブジェクトを更新
+    private void objUpdate()
+    {
+        for(int i = 0; i < chikinList.Count; i++)
+        {
+            // ここで死んでる奴は消しときましょう
+            if (!chikinList[i].isAlive())
+            {
+                chikinList.RemoveAt(i);
+                continue;
+            }
+            // 生きてたら動かしましょうね
+            chikinList[i].moveRondom();
+        }
+
+        // 拡張for文だと削除したときの関係でエラーが出るみたい
+        //foreach (chikin chik in chikinList)
+        //{
+        //    // ここで死んでる奴は消しときましょう
+        //    if (!chik.isAlive())
+        //    {
+        //        chikinList.Remove(chik);
+        //        continue;
+        //    }
+        //    // 生きてたら動かしましょうね
+        //    chik.moveRondom();
+        //}
+    }
+
+    // Instantiateが1MonoBehaviorのメンバだったのでmainで生成する。
+    // 制御自体はchikinクラス
+    private void createChiks(int x, int y)
+    {
+        chikin chik;
+        // Instantiateで生成したやつを保存します
+        chik = new chikin(Instantiate(chikObj, new Vector3 (x,y,0), Quaternion.identity) as GameObject);
+        // chikinのリストに入れます
+        chikinList.Add(chik);
+    }
+
+    // キー入力を監視
+    private void keyInput()
+    {
+        if(Input.GetKeyUp("space"))
+        {
+            createChiks(0,0);
+        }
+    }
+}
